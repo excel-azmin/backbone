690d2124d0523904625266cd03df4136efe5c407
Shaid Azmin <excel.azmin@gmail.com>
2025-06-14 15:47:59 +0600

    feature: add redis for registration

--- api-gateway/src/modules/auth/handlers/registation-handler.ts
+++ api-gateway/src/modules/auth/handlers/registation-handler.ts
@@ -1,5 +1,8 @@
+import { InjectRedis } from '@nestjs-modules/ioredis';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { Redis } from 'ioredis';
 import { generateOTP } from 'src/common/lib/otp/otp.service';
+import { buildCacheKey } from 'src/common/utils/cache';
 import {
   CreateNotificationDto,
   NotificationType,
@@ -11,9 +14,19 @@ import { RegistrationCommand } from '../command/registation-command';
 export class RegistrationHandler
   implements ICommandHandler<RegistrationCommand>
 {
-  constructor(private readonly notificationService: NotificationService) {}
+  constructor(
+    private readonly notificationService: NotificationService,
+    @InjectRedis() private readonly redis: Redis,
+  ) {}
   async execute(command: RegistrationCommand): Promise<any> {
     const { firstName, lastName, email } = command.registrationAuthDto;
+    const cachedData = this.redis.get(buildCacheKey('registration', { email }));
+    if (cachedData) {
+      return {
+        message: 'Registration already in progress, please check your email.',
+        statusCode: 400,
+      };
+    }
     const otp = generateOTP(6);
     const notificationPayload: CreateNotificationDto = {
       topic: 'User Registration Verification',
@@ -30,6 +43,12 @@ export class RegistrationHandler
       receiversId: [],
     };
     await this.notificationService.sendNotification(notificationPayload);
+    await this.redis.set(
+      buildCacheKey('registration', { email }),
+      JSON.stringify(command.registrationAuthDto),
+      'EX',
+      60 * 5,
+    );
     return {
       message: 'Registration successful, verification email sent.',
       statusCode: 201,
