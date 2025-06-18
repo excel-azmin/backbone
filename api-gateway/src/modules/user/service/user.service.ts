98673390131eeefff657250d5595f4775dba0e9d
Shaid Azmin <excel.azmin@gmail.com>
2025-06-18 12:44:02 +0600

    working on login response

--- api-gateway/src/modules/user/service/user.service.ts
+++ api-gateway/src/modules/user/service/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { BcryptService } from 'src/common/lib/bcrypt/bcypt.service';
+import { JsonWebTokenService } from 'src/common/lib/jwt-token/jsonwebtoken.service';
 import { CustomError } from 'src/common/shared/errors/custom-eror';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepository } from '../repository/user.repository';
@@ -9,6 +10,7 @@ export class UserService {
   constructor(
     readonly userRepository: UserRepository,
     private readonly bcryptService: BcryptService,
+    private readonly jsonWebTokenService: JsonWebTokenService,
   ) {}
 
   async createUser(createUser: CreateUserDto) {
@@ -30,13 +32,40 @@ export class UserService {
   }
 
   async isAlreadyExist(email: string) {
+    return await this.userRepository.findByEmail(email);
+  }
+
+  async validateLogin(email: string, password: string) {
     const user = await this.userRepository.findByEmail(email);
-    return user ? true : false;
+    if (!user) {
+      throw new CustomError('User not found', 404, {
+        message: 'User not found with this email.',
+        statusCode: 404,
+      });
+    }
+
+    const isPasswordValid = await this.bcryptService.compare(
+      password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new CustomError('Invalid password', 401, {
+        message: 'Invalid password.',
+        statusCode: 401,
+      });
+    }
+
+    const [access_token, refresh_token] = await Promise.all([
+      this.jsonWebTokenService.loginToken({ id: user.email }),
+      this.jsonWebTokenService.generateRefreshToken(user.email),
+    ]);
+
+    return { access_token, refresh_token };
   }
 
   async findUserByEmail(email: string) {
     try {
-      return this.userRepository.findByEmail(email);
+      return this.userRepository.findByEmail(email) ? true : false;
     } catch (error) {
       throw new CustomError('Failed to find user by email', 500, {
         message: error.message,
